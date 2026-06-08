29d8a3821a51e910fd2590fbdcef3e4eb56baec6
Raghu K <raghuk.vit@gmail.com>
2026-06-08 07:54:54 +0530

    add 12 file(s),update 1 file(s)

--- src/main/java/com/roboshop/orders/config/RestTemplateConfig.java
+++ src/main/java/com/roboshop/orders/config/RestTemplateConfig.java
@@ -13,8 +13,8 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
-                .connectTimeout(Duration.ofSeconds(3))
-                .readTimeout(Duration.ofSeconds(5))
+                .setConnectTimeout(Duration.ofSeconds(3))
+                .setReadTimeout(Duration.ofSeconds(5))
                 .build();
     }
 }
